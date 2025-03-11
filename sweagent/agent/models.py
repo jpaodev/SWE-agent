de87923052a86597b109600e4cb09865fbb9ac13
jpaodev <hello@jpao.dev>
2025-03-11 08:11:06 +0100

    test oai only

--- sweagent/agent/models.py
+++ sweagent/agent/models.py
@@ -662,6 +662,7 @@ class LiteLLMModel(AbstractModel):
         except Exception as e:
             print("Exception occurred {e}")
             print(traceback.format_exc())
+            raise
         except openai.BadRequestError as e:
             if "maximum context length" in str(e) or "context length exceeded" in str(e):
                 raise ContextWindowExceededError from e
@@ -782,17 +783,17 @@ class LiteLLMModel(AbstractModel):
         self, messages: list[dict[str, str]], n: int | None = None, temperature: float | None = None
     ) -> list[dict]:
         if n is None:
-            if os.getenv("USE_LITE_LLM", "false").lower() == "true":
-                return self._single_query(messages, temperature=temperature)
-            else:
-                return self._single_query_oai(messages, temperature=temperature)
+            # if os.getenv("USE_LITE_LLM", "false").lower() == "true":
+            #     return self._single_query(messages, temperature=temperature)
+            # else:
+            return self._single_query_oai(messages, temperature=temperature)
         outputs = []
         # not needed for openai, but oh well.
         for _ in range(n):
-            if os.getenv("USE_LITE_LLM", "false").lower() == "true":
-                outputs.extend(self._single_query(messages))
-            else:
-                outputs.extend(self._single_query_oai(messages))
+            # if os.getenv("USE_LITE_LLM", "false").lower() == "true":
+            #     outputs.extend(self._single_query(messages))
+            # else:
+            outputs.extend(self._single_query_oai(messages))
         return outputs
 
     def query(self, history: History, n: int = 1, temperature: float | None = None) -> list[dict] | dict:
